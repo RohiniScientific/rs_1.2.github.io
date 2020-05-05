7f8b8f2ce3f244fd6669891c869c9115c3d1db9d
Apoorva Grampurohit <apoo@apoorvas-mbp.lan>
2020-05-04 19:54:11 -0700

    RS Website Data

---
+++ js/hamburger_menu.js
@@ -0,0 +1,26 @@
+document.getElementById("hpmenuicon").onclick = function() {
+  document.getElementById("hpmenuicondiv").style.opacity = 0;
+  document.getElementById("hpmenuicondiv").style.zIndex = -2;
+  document.getElementById("hpmobilemenuwrapper").style.opacity = 1;
+  document.getElementById("hpmobilemenuwrapper").style.zIndex = 2;
+}
+
+document.getElementById('hpmenuxicon').onclick = function() {
+
+  document.getElementById("hpmobilemenuwrapper").style.opacity = 0;
+  document.getElementById("hpmobilemenuwrapper").style.zIndex = -2;
+  setTimeout(function() {
+    document.getElementById("hpmenuicondiv").style.opacity = 1;
+    document.getElementById("hpmenuicondiv").style.zIndex = 2;
+  }, 150);
+}
+
+document.getElementById("RS_showcase").onclick = function() {
+
+  document.getElementById("hpmobilemenuwrapper").style.opacity = 0;
+  document.getElementById("hpmobilemenuwrapper").style.zIndex = -2;
+  setTimeout(function() {
+    document.getElementById("hpmenuicondiv").style.opacity = 1;
+    document.getElementById("hpmenuicondiv").style.zIndex = 2;
+  }, 150);
+}
